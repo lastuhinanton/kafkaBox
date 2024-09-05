9fd73ae0b808a062b87e7ae8136a5aa2f31fe0e8
lastuhinanton <lastuhinanton74@gmail.com>
2024-09-05 02:13:22 -0400

    update the python script

--- src/python/helpers/parsers.py
+++ src/python/helpers/parsers.py
@@ -1,4 +1,5 @@
 import yaml
+import subprocess
 
 def detailed_check_creds_yml():
     creds  = parse_yaml('./creds.yml')
@@ -19,11 +20,11 @@ def detailed_check_creds_yml():
                         if not ki.isdigit():  raise Exception(f"Sorry, something wrong with 'ip={vm_keys['ip']}' of {vm}")
                         if int(ki) < 0 or int(ki) > 255: raise Exception(f"Sorry, something wrong with 'ip={vm_keys['ip']}' of {vm}")
 
-                    if not 'user' in vm_keys.keys(): raise Exception(f"Sorry, something wrong with 'user' field of {vm}")
-                    if vm_keys['user'] == None or vm_keys['user'].strip() == '':  raise Exception(f"Sorry, something wrong with 'user={vm_keys['user']}' of {vm}")
+                    if not 'username' in vm_keys.keys(): raise Exception(f"Sorry, something wrong with 'username' field of {vm}")
+                    if vm_keys['username'] == None or vm_keys['username'].strip() == '':  raise Exception(f"Sorry, something wrong with 'username={vm_keys['username']}' of {vm}")
 
-                    if not 'pass' in vm_keys.keys(): raise Exception(f"Sorry, something wrong with 'pass' of {vm}")
-                    if vm_keys['pass'] == None or vm_keys['pass'].strip() == '':  raise Exception(f"Sorry, something wrong with 'pass={vm_keys['pass']}' of {vm}")
+                    if not 'password' in vm_keys.keys(): raise Exception(f"Sorry, something wrong with 'password' of {vm}")
+                    if vm_keys['password'] == None or vm_keys['password'].strip() == '':  raise Exception(f"Sorry, something wrong with 'password={vm_keys['pass']}' of {vm}")
             else:
                 raise Exception("Sorry, creds.yml doesn't contain 'machines' or it's spelled")
     else:
@@ -33,3 +34,59 @@ def parse_yaml(path):
     with open(path) as f:
         yml = yaml.safe_load(f)
     return yml
+
+#TODO: optimize geting the parse_yaml
+def get_information(type_info, stand=None):
+    result = list()
+    creds = parse_yaml('./creds.yml')
+    project_options = parse_yaml('./ansible/environments/all/project_options.yml')
+    if type_info == 'kafka_versions':
+        result = get_kafka_versions(project_options)
+    elif type_info == 'zookeeper_versions':
+        result = get_zookeeper_versions(project_options)
+    elif type_info == 'stands':
+        result = get_stands(creds)
+    elif type_info == 'machines':
+        result = get_machines(creds, stand)
+    elif type_info == 'jdk_versions':
+        result = get_jdk_versions(project_options)
+    elif type_info == 'project_tasks':
+        result = get_project_tasks(project_options)
+    elif type_info == 'kafka_tasks':
+        result = get_kafka_tasks(project_options)
+    elif type_info == 'time':
+        result = get_current_time()
+    return result
+
+#TODO: make validation
+def get_kafka_versions(file):
+    return file['kafka_versions']
+
+def get_zookeeper_versions(file):
+    return file['zookeeper_versions']
+
+def get_stands(file):
+    return file['environments'].keys()
+
+def get_machines(file, stand):
+    result = dict()
+    machines = file['environments'][stand]['machines']
+    for machine in machines.keys():
+        result[machine] = dict()
+        result[machine]['id'] = machine.split('_')[1]
+        result[machine]['ip'] = machines[machine]['ip']
+        result[machine]['username'] = machines[machine]['username']
+        result[machine]['password'] = machines[machine]['password']
+    return result
+
+def get_jdk_versions(file):
+    return file['jdk_versions']
+
+def get_project_tasks(file):
+    return file['project_tasks']
+
+def get_kafka_tasks(file):
+    return file['kafka_tasks']
+
+def get_current_time():
+    return subprocess.run(['date', '+%d_%m_%Y_%H_%M_%S'], stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1]
--- src/python/main.py
+++ src/python/main.py
@@ -7,5 +7,35 @@ def start():
 	check_creds_yml()
 	introduce()
 	while True:
-		break
+		info = dict()
+		info['jdk_version'] = info['zookeeper_version'] = info['kafka_task'] = None
 
+		info['stand'] = choose_points('stands', 'Stands')
+
+		info['project_task'] = choose_points('project_tasks', 'Project tasks')
+		if info['project_task'] == 'env_setup':
+			info['jdk_version'] = choose_points('jdk_versions', 'JDK versions')
+
+		elif info['project_task'] == 'zookeeper':
+			info['zookeeper_version'] = choose_points('zookeeper_versions', 'Zookeeper versions')
+
+		elif info['project_task'] == 'kafka':
+			info['kafka_task'] = choose_points('kafka_tasks', 'Kafka tasks')
+			if info['kafka_task'] == 'install_kafka':
+				info['kafka_version'] = choose_points('kafka_versions', 'Kafka versions')
+
+		machine_option = choose_machines(info['stand'], 'Machines')
+		machines = get_information('machines', stand=info['stand'])
+		info['machines'] = machines
+		if machine_option == 'all':
+			for machine in machines.keys():
+				info['machine'] = machine
+				info['time'] = get_information('time')
+				check_json_external_vars(json.dumps(info, indent=4))
+				run_ansible(info)
+		else:
+			info['machine'] = machine_option
+			check_json_external_vars(json.dumps(info, indent=4))
+			run_ansible(info)
+
+		break
